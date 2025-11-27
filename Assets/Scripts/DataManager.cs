ed8e14ba0738f2cf854cc902bda2a98fbbda95ac
djsekai34 <davidjimenezvillena@gmail.com>
2025-11-27 19:21:41 +0100

    v0.5 Punto 4 terminado

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,4 +1,6 @@
-using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
@@ -6,27 +8,24 @@ public class DataManager : MonoBehaviour
     [SerializeField] private List<Item> items = new List<Item>();
     [SerializeField] private GameObject buttonContainer;
     [SerializeField] private ItemButtonManager itemButtonManager;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameManager.Instance.OnItemsMenu += CreateButtons;
+        GameManager.Instance.OnItemsMenu += CreateButtons; 
     }
 
-    // Update is called once per frame
     private void CreateButtons()
     {
-        foreach (var item in items)
+        foreach (var item in items) 
         {
             ItemButtonManager itemButton;
             itemButton = Instantiate(itemButtonManager, buttonContainer.transform);
-            itemButton.Itemname = item.ItemName;
-            itemButton.ItemDesrciption = item.ItemDescription;
-            itemButton.itemImage = item.ItemImage;
-            itemButton.item3DModel = item.Item3DModel;
+            itemButton.ItemName = item.ItemName;
+            itemButton.ItemDescription = item.ItemDescription;
+            itemButton.ItemImage = item.ItemImage;
+            itemButton.Item3DModel = item.Item3DModel;
             itemButton.name = item.ItemName;
         }
 
         GameManager.Instance.OnItemsMenu -= CreateButtons;
-
     }
 }
